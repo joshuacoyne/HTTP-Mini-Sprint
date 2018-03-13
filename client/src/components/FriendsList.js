2a0e207df5a1fd7998b346f475aa2a352e52b230
joshuacoyne <josh.j.coyne@gmail.com>
2018-03-13 00:06:39 -0400

    Completed

--- client/src/components/FriendsList.js
+++ client/src/components/FriendsList.js
@@ -2,6 +2,23 @@ import React, { Component } from 'react';
 import axios from 'axios';
 
 class FriendsList extends Component {
+
+  state = {
+    friends: []
+  };
+
+componentDidMount() {
+  axios
+    .get('http://localhost:5000/friends')
+    .then(response => {
+      this.setState(() => ({friends: response.data}));
+    })
+    .catch(error => {
+      console.error('Server Error:', error);
+    });
+    
+}
+    
   render() {
     return (
       <div>
